177d5e0038d1ce2bd978cba5e882b7fb5ceec216
guylain237 <72466774+guylain237@users.noreply.github.com>
2021-02-07 14:59:17 +0100

    Create exercice3.sql

---
+++ exercice3.sql
@@ -0,0 +1,1 @@
+CREATE VIEW EmploiDeTemps AS SELECT DISTINCT T.jourCoursDate, C.codeCours FROM Cours C JOIN Typehoraire T ON C.codeCours = T.crsCodeCours JOIN Jourcours J ON J.dateJourCours = T.jourCoursDate JOIN Coursdeclasse cls ON T.crsCodeCours = cls.crsCodeCours JOIN Classe Cl ON cl.specialiteNomSpec = cls.classSpecialiteNomspec ORDER BY T.jourCoursDate;
